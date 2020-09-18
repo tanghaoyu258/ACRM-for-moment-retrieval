9cad864a39d925dba2d635268c34074a9cab736b
tanghaoyu258 <44565959+tanghaoyu258@users.noreply.github.com>
2020-09-18 21:26:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,6 @@ def main():
     logger.info("Starting moment localization with dynamic filters")
     logger.info(cfg.EXPERIMENT_NAME)
 
-    # reproductibility
-    np.random.seed(0)
-    torch.manual_seed(0)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     # torch.backends.cudnn.enabled = False
